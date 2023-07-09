d17ffdd18f35a23b809d334dec2d440efdc5551e
jprogramer <joseph.debbah.local@gmail.com>
2023-07-09 16:31:10 +0100

    create a new html template on each update on a dedicated thread pool to support all html changes (not just variable updates) and to avoid any change for thread dead locking

--- src/jvmMain/kotlin/PdfGenerator.kt
+++ src/jvmMain/kotlin/PdfGenerator.kt
@@ -1,21 +1,28 @@
 import com.itextpdf.styledxmlparser.resolver.resource.IResourceRetriever
-import dz.nexatech.reporter.client.common.withIO
 import dz.nexatech.reporter.client.core.PdfConverter
 import kotlinx.coroutines.runBlocking
 import java.io.*
 import java.net.URL
+import java.util.concurrent.SynchronousQueue
+import java.util.concurrent.ThreadPoolExecutor
+import java.util.concurrent.TimeUnit
 
 class PdfGenerator(
-    configs: Configs,
+    val configs: Configs,
     val workDir: String,
     val inputFile: File,
 ) {
 
     companion object {
         private val log = logger()
+        private val executorService = ThreadPoolExecutor(
+            2, Runtime.getRuntime().availableProcessors() * 2,
+            60L, TimeUnit.SECONDS,
+            SynchronousQueue()
+        )
     }
 
-    private val templateParser: TemplateParser = TemplateParser(
+    private fun templateParser(): TemplateParser = TemplateParser(
         configs = configs,
         templateContentLoader = {
             if (it == inputFile.name) BufferedInputStream(FileInputStream(inputFile)) else null
@@ -52,16 +59,17 @@ class PdfGenerator(
         fontsLoader = { configs.loadFont(workDir) }
     )
 
-    fun generatePDF(output: File) {
-        runBlocking {
-            withIO {
-                try {
-                    val html = templateParser.evaluateState(inputFile.name)
-                    pdfConverter.generatePDF(BufferedOutputStream(FileOutputStream(output)), html)
-                    openFile(output)
-                } catch (e: Exception) {
-                    log.error("error while generating: ${output.absolutePath}", e)
+    fun generatePDF(destinationFile: File) {
+        executorService.submit {
+            try {
+                val html = templateParser().evaluateState(inputFile.name)
+                val outputStream = BufferedOutputStream(FileOutputStream(destinationFile))
+                runBlocking {
+                    pdfConverter.generatePDF(outputStream, html)
                 }
+                openFile(destinationFile)
+            } catch (e: Exception) {
+                log.error("error while generating: ${destinationFile.absolutePath}", e)
             }
         }
     }
